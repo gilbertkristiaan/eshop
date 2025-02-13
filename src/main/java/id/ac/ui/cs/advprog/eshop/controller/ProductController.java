7123bc47680fc82d184d5cf092a71ff1a19074b1
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-02-13 15:58:45 +0700

    [FEAT]: Optimized the code (error-handling, etc) and README for reflection 1

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -2,9 +2,11 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +26,8 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public String createProductPost(@ModelAttribute Product product, Model model) {
+    public String createProductPost(@Valid @ModelAttribute Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {return "CreateProduct";}
         service.create(product);
         return "redirect:list";
     }
@@ -39,12 +42,16 @@ public class ProductController {
     @GetMapping("/edit/{productId}")
     public String editProductPage(@PathVariable ("productId") String productId, Model model) {
         Product product = service.findById(productId);
+        if (product == null) {
+            return "redirect:/product/list";
+        }
         model.addAttribute("product", product);
         return "EditProduct";
     }
 
     @PostMapping("/edit/{productId}")
-    public String editProductPost(@PathVariable("productId") String productId, @ModelAttribute Product product) {
+    public String editProductPost(@PathVariable("productId") String productId, @Valid @ModelAttribute Product product, BindingResult result) {
+        if (result.hasErrors()) {return "EditProduct";}
         service.update(productId, product);
         return "redirect:/product/list";
     }
