976e76572ee3349dea7e869ed6860bd9680b7d7b
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-02-20 13:33:34 +0700

    [FIX] : Fix the mapping

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -36,7 +36,7 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{productId}")
